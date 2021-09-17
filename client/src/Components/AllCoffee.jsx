469ad47931441304c92d2812f7cd67294e604073
digallardox <digallardox@gmail.com>
2021-09-17 10:25:50 -0500

    third commit

---
+++ client/src/Components/AllCoffee.jsx
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
+
+let airtableBase = process.env.REACT_APP_AIRTABLE_BASE;
+let airtableKey = process.env.REACT_APP_AIRTABLE_KEY;
+
+const URL = `https://api.airtable.com/v0/${airtableBase}/Cafe%20Loco`;
+
+const config = {
+        headers: {
+            Authorization: `Bearer ${airtableKey}`,
+        }
+    }
+
+function AllCoffee() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const getData = async () => {
+        const res = await axios.get(URL, config);
+        setData(res.data.records)
+        console.log(data);
+        }
+        getData();
+    },[])
+
+    
+    return (
+        <div>
+
+        {data.map((item) => {
+            return (
+                <div>
+                <h3>{item.fields.name}</h3>
+                <p>{item.fields.notes}</p>
+                </div>
+            )
+        })}
+        </div>
+    )
+};
+
+export default AllCoffee;
---
+++ client/src/Components/CoverImg.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+import { Image } from 'react-bootstrap';
+
+function CoverImg(){
+    return (
+        <div>
+            <Image src='https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80' fluid />
+        </div>
+    )
+}
+
+export default CoverImg;
